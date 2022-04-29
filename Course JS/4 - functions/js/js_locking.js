b1f5ae2887fe102de507135346c81ff81a563cf7
Yevhen Shulha <yevhen.shulha@avanquest.com>
2022-04-29 19:04:48 +0300

    added functions

--- Course JS/4 - functions/js/js_locking.js
+++ Course JS/4 - functions/js/js_locking.js
@@ -39,11 +39,67 @@
 // }
 // console.log(aFn(2)(19)); // 21
 // // OR
-function aFn(a){
-    return function(b){
-        return a + b;
-    }
-}
- let sum = aFn(10);
- console.log(sum(22));
+// function aFn(a){
+//     return function(b){
+//         return a + b;
+//     }
+// }
+//  let sum = aFn(10);
+//  console.log(sum(22));
  
+
+// Задача 4 - периметр треугольника
+// function perimetrFn(a, b) {
+//     return function(c) {
+//         return a + b + c;
+//     }
+// }
+// let res = perimetrFn(10, 2);
+// console.log(res(23));
+// // OR
+// function calcPerimetr(a, b, c) {
+//     return a + b + c;
+// }
+// console.log(calcPerimetr(10, 11, 12));
+
+
+// // 5) Должна вывестись посимвольно строка (второй параметр) 
+// 2 парметра в фнукции первый - это функция, которая делает uppercase каждый символ строки, второй параметр - это строка
+// fn
+function toUpper(symbol) {
+    console.log(symbol.toUpperCase());
+}
+
+function useToUppercase(fn, str) {
+    // // for...of -> можно пройтись по элементам строки
+    // // Проходится по каждому символу строки str и сохраняет на каждой итерации этот символ в переменную index
+    for (let index of str) {
+        // console.log(index);
+        //s
+        //o
+        //m
+        //e
+        // 
+        //s
+        // .....
+        //g
+        // А теперь используем (вызываем) функцию useToUppercase с параметром (fn), где этот параметр и есть функцией toUpper -> useToUppercase(toUpper, 'some string');
+        // исключить пробелы
+        // if ( index === ' ' ) {
+        //     continue;
+        // }
+        // OR
+         // if ( index !== ' ' ) {
+        //     fn(index);
+        // }
+        fn(index);
+        }
+        
+    // // OR
+    // for ( let i = 0; i < str.length; i++) {
+    //     // console.log(str[i]);
+    //     fn(str[i]);
+    // }
+
+}
+useToUppercase(toUpper, 'some string');
