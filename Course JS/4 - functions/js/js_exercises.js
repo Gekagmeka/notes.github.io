282932c5939948cc3f6817e4f58b400ca4e1badd
Yevhen Shulha <yevhen.shulha@avanquest.com>
2022-05-04 23:03:26 +0300

    added stickers

--- Course JS/4 - functions/js/js_exercises.js
+++ Course JS/4 - functions/js/js_exercises.js
@@ -19,3 +19,16 @@
 // }
 
 // console.log(getNumbers(0, 4));
+
+
+
+// // 2) Создайте функцию sum, которая будет возвращать сумму любого количества чисел. Вызываться функция должна вот так (обратите внимание на скобки): x = sum(2)(5)(10)
+//a)
+// function sum(...args) {
+//     let result = 0;
+//     for ( let i = 0; i < args.length; i++){
+//         result = result + args[i];
+//     }
+//     console.log(result);
+// }
+// sum(1,2,3);
